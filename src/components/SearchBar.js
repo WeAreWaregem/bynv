7f5b2c98f22522b95692b054d2254b18593111b4
WeAreWaregem <vromanmathieu@gmail.com>
2022-02-26 14:59:32 +0100

    try api call

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,6 +4,21 @@ import './SearchBar.css';
 export default function SearchBar(){
     const [releasesList, fetchReleases] = useState([]);
     const [imgList, fetchImages] = useState([]);
+    const coverList = [];
+
+    async function test(album){
+        fetch("https://coverartarchive.org/release/81ae60d4-5b75-38df-903a-db2cfa51c2c6")
+        .then((res) => res.json())
+        .then((res) => {
+            console.log(res)
+            fetchImages(res.images)
+        })
+        .thencoverList.push(imgList[0])
+    }
+    
+    async function test2(inputlist) {
+        coverList.push(inputlist[0])
+    }
 
     const search = () => {
         fetch("https://musicbrainz.org/ws/2/release/?artist=0383dadf-2a4e-4d10-a46a-e9e041da8eb3&inc=recordings&fmt=json")
@@ -12,25 +27,17 @@ export default function SearchBar(){
                 console.log(res)
                 fetchReleases(res.releases)
             })
-        }
+        
+            for(var i=0; i < releasesList.length; i++){
 
-    const searchImg = (releaseID) => {
-        const imgUrl = `https://coverartarchive.org/release/"${releaseID}"`
-        fetch(imgUrl)
-            .then((res) => res.json())
-            .then((res) => {
-                console.log(res)
-                fetchImages(res.images)
-            })
+                test(releasesList[i])
+                test2(imgList)          
         }
+    }
 
     useEffect(() => {
         search()
     }, [])
-
-    // useEffect(() => {
-    //     searchImg()
-    // }, [])
 
     return (
         <> 
@@ -41,17 +48,18 @@ export default function SearchBar(){
                 <input className="input" type="text" name="query"/>
                 <button className="button" type="submit">Search</button>
             </form>
+            
             <div className="card-list">
-                {releasesList.map((item, i) => {
-                    searchImg(item.id)
-                    imgList.map((img, j) =>  {
-                        return <div key={j}>{img.image}</div>
-                    })
+                {/* {releasesList.map((item, i) => {
                         return <li key={i}>{item.id}</li>
+                })} */}
+
+                {coverList.length}
+
+                {coverList.map((item, i) => {
+                        return <img src={item.image} alt='movie'></img>
                 })}
             </div>
         </>
 
-    )
-
-}
+    )}
